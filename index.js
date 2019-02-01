ca911f94672d78dd6731c559cadfd06554ffedd3
Susan Tan <susan.tan198@gmail.com>
2019-02-01 19:27:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,31 +10,20 @@ function logShout(str){
   console.log(str.toUpperCase())
 }
 
+function  logWhisper(str){
+  console.log(str.toLowerCase());
+}
+
+function sayHiToGrandma(str){
+  if(str === str.toLowerCase()){
+    return "I can\' hear you!"
+  } else if(str. === str.toUpperCase()){
+    return "YES INDEED!"
+  }
+}
 
 /*
-describe('logShout(string)', function() {
-  it('calls console.log() its one argument in all caps', function() {
-    const spy = expect.spyOn(console, 'log').andCallThrough()
 
-    logShout('hello')
-
-    expect(spy).toHaveBeenCalledWith('HELLO')
-
-    console.log.restore()
-  })
-})
-
-describe('logWhisper(string)', function() {
-  it('calls console.log() its one argument in all lowercase', function() {
-    const spy = expect.spyOn(console, 'log').andCallThrough()
-
-    logWhisper('HELLO')
-
-    expect(spy).toHaveBeenCalledWith('hello')
-
-    console.log.restore()
-  })
-})
 
 describe('sayHiToGrandma(string)', function() {
   it('returns "I can\'t hear you!" if `string` is lowercase', function() {
