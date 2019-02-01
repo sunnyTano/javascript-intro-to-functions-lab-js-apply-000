4057f069cf3aa590675145e7f9f5d8a6e9b5b993
Susan Tan <susan.tan198@gmail.com>
2019-02-01 19:20:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
+function shout(str){
+  return shout.toUpperCase();
+}
+
+function whisper(str){
+  return str.toLowercase();
+}
 
 
 
-/*describe('shout(string)', function() {
-  it('receives one argument and returns it in all caps', function() {
-    expect(shout('hello')).toEqual('HELLO')
-  })
-})
 
-describe('whisper(string)', function() {
-  it('receives one argument and returns it in all lowercase', function() {
-    expect(whisper('HELLO')).toEqual('hello')
-  })
-})
+/*
+
 
 describe('logShout(string)', function() {
   it('calls console.log() its one argument in all caps', function() {
